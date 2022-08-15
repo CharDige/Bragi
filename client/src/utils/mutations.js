54eeeb09ad22b9bd1dddc7ee062175e075660dcc
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-08-15 21:23:14 +0930

    Added channel and genre into AddUser mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,13 +1,15 @@
 import { gql } from '@apollo/client';
 
 export const ADD_USER = gql`
-mutation AddUser($username: String!, $email: String!, $password: String!) {
-  addUser(username: $username, email: $email, password: $password) {
+mutation AddUser($username: String!, $email: String!, $password: String!, $channel: String!, $genre: String!) {
+  addUser(username: $username, email: $email, password: $password, channel: $channel, genre: $genre) {
     token
     user {
       _id
       username
       email
+      channel
+      genre
     }
   }
 }`
