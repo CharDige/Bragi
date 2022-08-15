ec7dd99260dc21a4f76abc87bf71d8b42d8ae202
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-08-15 21:22:52 +0930

    Added queries currently in server into front-end

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,11 +1,72 @@
 import { gql } from '@apollo/client';
 
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      channel
+      genre
+      stories {
+        _id
+        storyTitle
+        storyDescription
+        storyGenre
+        createdAt
+      }
+    }
+  }
+`;
+
+export const QUERY_STORIES = gql`
+  query getStories {
+    stories {
+      _id
+      storyTitle
+      storyDescription
+      storyAuthor
+      storyGenre
+      createdAt
+    }
+  }
+`;
+
+export const QUERY_SINGLE_STORY = gql`
+  query getSingleStory($storyId: ID!) {
+    story(storyId: $storyId) {
+      _id
+      storyTitle
+      storyDescription
+      storyContent
+      storyAuthor
+      storyGenre
+      createdAt
+      comments {
+        _id
+        commentText
+        commentAuthor
+        createdAt
+      }
+    }
+  }
+`;
+
 export const QUERY_ME= gql`
 query Me {
   me {
     _id
     username
     email
+    channel
+    genre
+    stories {
+      _id
+      storyTitle
+      storyDescription
+      storyGenre
+      createdAt
+    }
   }
 }
 `;
