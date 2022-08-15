884037237639a982612669798d3c5a731ce98b89
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-08-15 20:47:27 +0930

    Added user, stories and story queries to test

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,23 +1,33 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
+const { User, Story } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
     users: async () => {
-      return User.find();
+      return User.find().populate('stories');
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('stories');
+    },
+    stories: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Story.find(params).sort({ createdAt: -1 });
+    },
+    story: async (parent, { storyId }) => {
+      return Story.findOne({ _id: storyId });
     },
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ email: context.user.email});
+        return User.findOne({ email: context.user.email}).populate('stories');
       }
       throw new AuthenticationError('You must be signed in');
     },
   },
 
   Mutation: {
-    addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
+    addUser: async (parent, { username, email, password, channel, genre }) => {
+      const user = await User.create({ username, email, password, channel, genre });
       const token = signToken(user);
       return { token, user };
     },
