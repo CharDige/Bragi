c6d66d429b69f077983f21a482e798c67d7688f8
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-08-16 21:40:02 +0930

    Updated code to display different profile pages depending on the route, and the test the form

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,24 @@
 import { useQuery } from '@apollo/client';
 import React from 'react';
-import { QUERY_ME } from '../utils/queries';
+import { Navigate, useParams } from 'react-router-dom';
+
+import StoryForm from '../components/StoryForm';
+
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
+
+import Auth from '../utils/auth';
 
 const Profile = () => {
-    const {loading, data} = useQuery(QUERY_ME);
+    const { username: userParam } = useParams();
+    const {loading, data} = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+        variables: { username: userParam },
+    });
+
+    const user = data?.me || data?.user || {};
+
+    if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+        return <Navigate to="/profile" />;
+    }
 
     if (loading) {
         return (
@@ -13,17 +28,28 @@ const Profile = () => {
         )
     }
 
-    const profileData = data?.me || {}
+    if (!user?.username) {
+        return (
+            <p>
+                Whoops! You need to be logged in to see a profile.
+            </p>
+        );
+    }
 
     return (
-        <>
-            <p>Profile Page</p>
-            Id: {profileData._id}
-            <br/>
-            Email: {profileData.email}
-            <br/>
-            Username: {profileData.username}
-        </>
+        <div>
+            <div className="flex-row justify-center mb-3">
+                <h2 className="col-12 col-lg-10">
+                    {userParam ? `${user.username}` : 'You'}
+                </h2>
+
+                {!userParam && (
+                    <div className="col-12 col-lg-10 mb-3 p-3">
+                        <StoryForm />
+                    </div>
+                )}
+            </div>
+        </div>
     );
 };
 
