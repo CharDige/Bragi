da16b58e0dbbb48a3aacbd68a723bc54137b6d9f
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-08-18 20:47:15 +0930

    Imported required functions and created SingleStory page to return when called

---
+++ client/src/pages/SingleStory.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+// Import useParams()
+import { useParams } from 'react-router-dom';
+
+// Import useQuery()
+import { useQuery } from '@apollo/client';
+
+// Import queries
+import { QUERY_SINGLE_STORY } from '../utils/queries';
+
+const SingleStory = () => {
+    const { storyId } = useParams();
+
+    const { loading, data } = useQuery(QUERY_SINGLE_STORY, {
+        variables: { storyId: storyId },
+    });
+
+    const story = data?.story || {};
+
+    if (loading) {
+        return <p>Loading...</p>
+    }
+
+    return (
+        <div className="my-3">
+            <div className="card-header p-2 m-0">
+                <h2>
+                    {story.storyTitle} by {story.storyAuthor}
+                </h2>
+                <p>Published on {story.createdAt}</p>
+            </div>
+            <div className='card-body'>
+                <p>
+                    {story.storyContent}
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default SingleStory;
