095af6d96ecc6e7533adc4fc95d2426790c395e8
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-08-18 21:14:02 +0930

    Added ability to delete stories in the story list

--- client/src/components/StoryList/index.js
+++ client/src/components/StoryList/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+
+// Import Auth
+import Auth from '../../utils/auth';
+
+// Add mutations
+import { REMOVE_STORY } from '../../utils/mutations';
 
 const StoryList = ({
     stories
 }) => {
+    const [removeStory] = useMutation(REMOVE_STORY)
+
+    const handleDeleteStory = async (storyId) => {
+        const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+        if (!token) {
+            return false;
+        }
+
+        try {
+            const { data } = await removeStory({
+                variables: {storyId}
+            });
+            window.location.reload();
+        } catch (error) {
+            console.log(error)
+        };
+    }
+
     if (!stories.length) {
         return <p>No stories yet!</p>
     }
@@ -34,6 +60,9 @@ const StoryList = ({
                         >
                             Read more
                         </Link>
+                        <button className='btn' onClick={() => handleDeleteStory(story._id)}>
+                            Remove story
+                        </button>
                     </div>
                 ))}
         </div>
