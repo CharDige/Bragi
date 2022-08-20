7797c4e95a292a209b9f344ebc630717b198d16b
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-08-20 10:55:41 +0930

    Added homepage HTML and query to list all stories

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+
+// Import StoryList component
+import StoryList from '../components/StoryList';
+
+// Import queries
+import { QUERY_STORIES } from '../utils/queries';
 
 const Home = () => {
+  const { loading, data } = useQuery(QUERY_STORIES);
+  const stories = data?.stories || [];
 
   return (
     <main>
       <div className="flex-row justify-center">
-        homepage
+        <div className="col-12 col-lg-10 mb-3 p-3">
+          {loading ? (
+            <p>Loading stories...</p>
+          ) : (
+            <StoryList
+              stories={stories}
+            />
+          )}
+        </div>
       </div>
     </main>
   );
