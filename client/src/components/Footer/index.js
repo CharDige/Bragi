4f48804c7ac357a574acbf79e63a5c0a7169bbe2
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-08-20 17:17:09 +0930

    Removed unused class

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="footer-styling">
+        <footer>
             <div className="container text-center mb-5">
                 <p>&copy; Charlotte Dige 2022</p>
             </div>
