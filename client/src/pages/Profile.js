b2af88339d3053c4236eb66219a7fdd348785664
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-08-20 19:55:56 +0930

    Added in authorisation requirement

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,6 @@
 import { useQuery } from '@apollo/client';
 import React from 'react';
-import { Navigate, useParams } from 'react-router-dom';
+import { Navigate, Link, useParams } from 'react-router-dom';
 
 import StoryForm from '../components/StoryForm';
 import StoryList from '../components/StoryList';
@@ -40,49 +40,56 @@ const Profile = () => {
 
     return (
         <main>
-            <div>
-                <div className="row justify-center mb-3">
-                    <div className='col-12 col-lg-3'>
-                        <h2>
-                            {userParam ? `${user.username}` : 'Your profile'}
-                        </h2>
+            {Auth.loggedIn() ? (
+                <div>
+                    <div className="row justify-center mb-3">
+                        <div className='col-12 col-lg-3'>
+                            <h2>
+                                {userParam ? `${user.username}` : 'Your profile'}
+                            </h2>
 
-                        <h3>
-                            Preferred channel
-                        </h3>
-                        <p className='profile-details'>
-                            {user.channel}
-                        </p>
+                            <h3>
+                                Preferred channel
+                            </h3>
+                            <p className='profile-details'>
+                                {user.channel}
+                            </p>
 
-                        <h3>
-                            Preferred genre
-                        </h3>
-                        <p className='profile-details'>
-                            {user.genre}
-                        </p>
-                    </div>
-
-                    <div className='col-12 col-lg-9'>
-                        <h2>
-                            Stories published
-                        </h2>
-
-                        <div>
-                            <StoryList
-                                stories={user.stories}
-                                showUsername={false}
-                                showButton={!userParam}
-                            />
+                            <h3>
+                                Preferred genre
+                            </h3>
+                            <p className='profile-details'>
+                                {user.genre}
+                            </p>
                         </div>
 
-                        {!userParam && (
-                            <div className="col-12 col-lg-10 mb-3 p-3">
-                                <StoryForm />
+                        <div className='col-12 col-lg-9'>
+                            <h2>
+                                Stories published
+                            </h2>
+
+                            <div>
+                                <StoryList
+                                    stories={user.stories}
+                                    showUsername={false}
+                                    showButton={!userParam}
+                                />
                             </div>
-                        )}
+
+                            {!userParam && (
+                                <div className="col-12 col-lg-10 mb-3 p-3">
+                                    <StoryForm />
+                                </div>
+                            )}
+                        </div>
                     </div>
                 </div>
-            </div>
+            ) : (
+                <p className='error-message'>
+                    Whoops! You need to be logged in to look at profiles. Please{' '}
+                    <Link to="/login" className='login-signup-link'>login</Link> or <Link to="/signup" className='login-signup-link'>signup.</Link>
+                </p>
+            )}
         </main>
     );
 };
