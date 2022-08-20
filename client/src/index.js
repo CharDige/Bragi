ff91e1e90d0b7f5b82e0b19da1fa0b1a8b7d9a19
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-08-20 14:54:47 +0930

    Updated import for styling

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 ReactDOM.render(
   <React.StrictMode>
