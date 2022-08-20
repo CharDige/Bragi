a058978bae8318e699ef1fb0495f82a3b1cacc3c
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-08-20 19:32:57 +0930

    Updated HTML and classes for styling

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -41,53 +41,36 @@ const Login = (props) => {
   };
 
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-lg-10">
-        <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Login</h4>
-          <div className="card-body">
-            {data ? (
-              <p>
-                Success! You may now head{' '}
-                <Link to="/">back to the homepage.</Link>
-              </p>
-            ) : (
-              <form onSubmit={handleFormSubmit}>
-                <input
-                  className="form-input"
-                  placeholder="Your email"
-                  name="email"
-                  type="email"
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  value={formState.password}
-                  onChange={handleChange}
-                />
-                <button
-                  className="btn btn-block btn-primary"
-                  style={{ cursor: 'pointer' }}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
-            )}
-
-            {error && (
-              <div className="my-3 p-3 bg-danger text-white">
-                {error.message}
-              </div>
-            )}
-          </div>
+    <main>
+      {data ? (
+        <div className='success-message'>
+          <p>
+            Success! You may now head{' '}
+            <Link to="/">back to the homepage.</Link>
+          </p>
         </div>
-      </div>
-    </main>
+      ) : (
+        <div className="login-signup-form-style">
+          <form onSubmit={handleFormSubmit}>
+            <div className="mb-3">
+              <label className="form-label">Email</label>
+              <input type="email"
+                name="email" className="form-control form-input-style" id="email" placeholder="Your email" value={formState.email} onChange={handleChange} />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Password</label>
+              <input type="password" name="password" className="form-control form-input-style" id="password" placeholder='********' value={formState.password} onChange={handleChange} />
+            </div>
+            <button type="submit" className="btn login-signup-btn-style">Submit</button>
+          </form>
+        </div>
+      )}
+      {error && (
+        <div className="my-3 p-3 bg-danger text-white">
+          {error.message}
+        </div>
+      )}
+    </main >
   );
 };
 
