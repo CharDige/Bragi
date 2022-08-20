c9bedaf27a7759a22a6d625d362a1e2293f5df58
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-08-20 17:09:35 +0930

    Updated classes and html structure for styling

--- client/src/components/StoryList/index.js
+++ client/src/components/StoryList/index.js
@@ -39,46 +39,52 @@ const StoryList = ({
     return (
         <div>
             <div className='row'>
-                    {stories &&
-                        stories.map((story) => (
-                            <div key={story._id} className="col-12 col-lg-6">
-                                <div className="card mb-3">
-                                    <h4 className="card-header p-2 m-0">
+                {stories &&
+                    stories.map((story) => (
+                        <div key={story._id} className="col-12 col-lg-6">
+                            <div className="card-styling">
+                                <div className="story-card-header">
+                                    <h3 className='h3-extra-colour'>
                                         <Link
                                             to={`/stories/${story._id}`}
+                                            className="story-title-style"
                                         >
                                             {story.storyTitle}
                                         </Link>
                                         {showUsername ? (
-                                            <span>{' '}by <Link to={`/profiles/${story.storyAuthor}`}>
+                                            <span>{' '}by <Link to={`/profiles/${story.storyAuthor}`} className="story-title-style">
                                                 {story.storyAuthor}
-                                            </Link></span>
+                                            </Link>
+                                            </span>
                                         ) : (
                                             <span></span>
                                         )}
-                                    </h4>
-                                    <div className="card-body p-2">
-                                        <p>
-                                            {story.storyDescription}
-                                        </p>
-                                    </div>
-                                    <Link
-                                        className="btn"
-                                        to={`/stories/${story._id}`}
-                                    >
-                                        Read more
-                                    </Link>
-                                    {showButton ? (
-                                        <button className='btn' onClick={() => handleDeleteStory(story._id)}>
-                                            Remove story
-                                        </button>
-                                    ) : (
-                                        <span></span>
-                                    )}
+                                    </h3>
+                                    <p className='created-date'>{' '}created on {story.createdAt}</p>
                                 </div>
 
+                                <div className="story-card-body">
+                                    <p>
+                                        {story.storyDescription}
+                                    </p>
+                                </div>
+                                <Link
+                                    className="btn btn-style"
+                                    to={`/stories/${story._id}`}
+                                >
+                                    Read more
+                                </Link>
+                                {showButton ? (
+                                    <button className='btn btn-style' onClick={() => handleDeleteStory(story._id)}>
+                                        Remove story
+                                    </button>
+                                ) : (
+                                    <span></span>
+                                )}
                             </div>
-                        ))}
+
+                        </div>
+                    ))}
             </div>
         </div>
     );
