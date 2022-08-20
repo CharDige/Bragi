3bc24e15f975d4182893704d9efc5aa651aefd1a
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-08-20 17:59:53 +0930

    Updated classes for styling:

--- client/src/components/StoryForm/index.js
+++ client/src/components/StoryForm/index.js
@@ -89,31 +89,31 @@ const StoryForm = () => {
             {Auth.loggedIn() ? (
                 <>
                     <form className="flex-row justify-center justify-space-between-md align-center" onSubmit={handleFormSubmit}>
-                        <div className="col-12 col-lg-9">
+                        <div className="col-12 form-input-margin">
                             <div className="mb-3">
                                 <label htmlFor="storyTitle" className="form-label label-style">Story title</label>
-                                <textarea value={storyTitle} name="storyTitle" className="form-control input-style" id="storyTitle" placeholder="Story title" onChange={handleInputChange}></textarea>
+                                <textarea value={storyTitle} name="storyTitle" className="form-control input-style form-input-style" id="storyTitle" placeholder="Story title" onChange={handleInputChange}></textarea>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="storyDescription" className="form-label label-style">Story description</label>
-                                <textarea value={storyDescription} name="storyDescription" className="form-control input-style" id="storyDescription" placeholder="Story description" onChange={handleInputChange}></textarea>
+                                <textarea value={storyDescription} name="storyDescription" className="form-control input-style form-input-style" id="storyDescription" placeholder="Story description" onChange={handleInputChange} rows="3"></textarea>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="storyContent" className="form-label label-style">Story content</label>
-                                <textarea value={storyContent} name="storyContent" className="form-control input-style" id="storyContent" placeholder="Story content" onChange={handleInputChange}></textarea>
+                                <textarea value={storyContent} name="storyContent" className="form-control input-style form-input-style" id="storyContent" placeholder="Story content" onChange={handleInputChange} rows="10"></textarea>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="storyGenre" className="form-label label-style">Story genre</label>
-                                <textarea value={storyGenre} name="storyGenre" className="form-control input-style" id="storyGenre" placeholder="Story genre, like 'Action', 'Horror', 'Satire', etc" onChange={handleInputChange}></textarea>
+                                <textarea value={storyGenre} name="storyGenre" className="form-control input-style form-input-style" id="storyGenre" placeholder="Story genre, like 'Action', 'Horror', 'Satire', etc" onChange={handleInputChange}></textarea>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="storyChannel" className="form-label label-style">Story channel</label>
-                                <textarea value={storyChannel} name="storyChannel" className="form-control input-style" id="storyChannel" placeholder="Story channel, like 'Book', 'Film', 'Video game', etc" onChange={handleInputChange}></textarea>
+                                <textarea value={storyChannel} name="storyChannel" className="form-control input-style form-input-style" id="storyChannel" placeholder="Story channel, like 'Book', 'Film', 'Video game', etc" onChange={handleInputChange}></textarea>
                             </div>
                         </div>
 
-                        <div className="col-12 col-lg-3">
-                            <button className="btn" type="submit">
+                        <div className="col-12">
+                            <button className="btn btn-style" type="submit">
                                 Add story
                             </button>
                         </div>
