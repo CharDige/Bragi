5bb1fd15cfa283c3113e1dea39d6a046a8c469bb
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-08-20 17:16:39 +0930

    Updated to remove back button

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,20 +1,9 @@
 import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
 
 const Footer = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
     return (
-        <footer className="w-100 mt-auto bg-secondary p-4">
+        <footer className="footer-styling">
             <div className="container text-center mb-5">
-                {location.pathname !== '/' && (
-                    <button
-                        className="btn btn-dark mb-3"
-                        onClick={() => navigate(-1)}
-                    >
-                        &larr; Go Back
-                    </button>
-                )}
                 <p>&copy; Charlotte Dige 2022</p>
             </div>
         </footer>
